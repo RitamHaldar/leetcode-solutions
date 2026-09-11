c10eb6cf1b940cc40d887c83ddbd1227f3ddb778
Ritam Haldar <rickhaldar561@gmail.com>
2026-09-11 14:24:52 +0000

    Sync LeetCode submission Runtime - 3 ms (34.35%), Memory - 77.1 MB (45.85%)

--- 0002-add-two-numbers/solution.cpp
+++ 0002-add-two-numbers/solution.cpp
@@ -11,33 +11,38 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode dummy(0);  
-        ListNode* current = &dummy;  
+        ListNode *temp=new ListNode(100);
+        ListNode *temph=temp;
+        string num1="";
+        string num2="";
+        while(l1!=NULL){
+            num1+=to_string(l1->val);
+            l1=l1->next;
+        }
+        while(l2!=NULL){
+            num2+=to_string(l2->val);
+            l2=l2->next;
+        }
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
         int carry = 0;
-
-    
-        while (l1 != nullptr || l2 != nullptr) {
-
-            int x = (l1 != nullptr) ? l1->val : 0;  
-            int y = (l2 != nullptr) ? l2->val : 0; 
-
-
-            int sum = carry + x + y;  
-            carry = sum / 10;  
-
-            current->next = new ListNode(sum % 10); 
-            current = current->next;  
-
-            if (l1 != nullptr) l1 = l1->next; 
-            if (l2 != nullptr) l2 = l2->next;  
-
+        string sum = "";
+        while (i >= 0 || j >= 0 || carry) {
+            int x = (i >= 0) ? num1[i] - '0' : 0;
+            int y = (j >= 0) ? num2[j] - '0' : 0;
+            int s = x + y + carry;
+            sum += to_string(s % 10);
+            carry = s / 10;
+            i--;
+            j--;
         }
-
-        
-        if (carry > 0) {
-            current->next = new ListNode(carry);
+        for (int i = 0; i < sum.size(); i++) {
+            ListNode *t = new ListNode(sum[i] - '0');
+            temph->next = t;
+            temph = temph->next;
         }
-
-        return dummy.next;  
+        return temp->next;
     }
 };
