970f01818c3ccf5cc10bb2ecbb04d18c78fdbf8b
Ritam Haldar <rickhaldar561@gmail.com>
2026-09-15 06:39:15 +0000

    Sync LeetCode submission Runtime - 24 ms (52.94%), Memory - 139.1 MB (73.71%)

--- 0239-sliding-window-maximum/solution.cpp
+++ 0239-sliding-window-maximum/solution.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
+        if (k==1) return nums;
+        deque<int> q;
         vector<int> ans;
-        vector<int> ngi(n);
-        stack<int> st;
-        ngi[n-1]=n;
-        st.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(st.size()>0 && nums[st.top()]<=nums[i]) st.pop();
-            if(st.size()==0) ngi[i]=n;
-            else ngi[i]=st.top();
-            st.push(i);
-        }
-        int j=0;
-        for(int i=0;i<n-k+1;i++){
-            if(j<i) j=i;
-            int mx=nums[j];
-            while(j<i+k){
-                mx=nums[j];
-                if(ngi[j]>=i+k) break;
-                j=ngi[j];
-            }
-            ans.push_back(mx);
+        for (int i=0;i<nums.size();i++){
+            while(q.size()>0 && nums[q.back()]<nums[i]) q.pop_back();
+            q.push_back(i);
+            int j=i-k+1;
+            while(q.front()<j) q.pop_front();
+            if(i>=k-1) ans.push_back(nums[q.front()]);
         }
         return ans;
+
     }
 };
